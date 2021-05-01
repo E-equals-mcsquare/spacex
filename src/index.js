117b8adec03c457d6035a132278ba065b6267831
Souvik Majumder <souvikmajumder@Souviks-MacBook-Air.local>
2021-05-01 19:13:26 +0530

    V1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {Provider} from 'react-redux'
 import store from './stores/StandardStore';
 import reportWebVitals from './reportWebVitals';
+
+var React = require('react');
+var ReactDOM = require('react-dom');
 
 ReactDOM.hydrate(
     <Provider store={store}>
