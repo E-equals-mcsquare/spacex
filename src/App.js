0a2082b0682177b0f83bbd66d553d4e1d896b54a
Souvik Majumder <souvikmajumder@Souviks-MacBook-Air.local>
2021-05-01 17:06:22 +0530

    V2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 import { Switch, Route, HashRouter as Router } from "react-router-dom";
 import Dashboard from "./components/dashboard/Dashboard";
-import Footer from "./footer/Footer";
-import Header from "./header/Header";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
 
 function App() {
   return (
