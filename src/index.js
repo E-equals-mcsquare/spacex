6e712180c1923d9050a102843eaf492849266a95
Souvik Majumder <souvikmajumder@Souviks-MacBook-Air.local>
2021-05-02 19:29:12 +0530

    V1 with Unit testing enabled

--- src/index.js
+++ src/index.js
@@ -1,28 +1,19 @@
-// var React = require('react');
-// import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {Provider} from 'react-redux'
 import store from './stores/StandardStore';
 import reportWebVitals from './reportWebVitals';
 
-var React = require('react');
-var ReactDOM = require('react-dom');
-
-ReactDOM.hydrate(
+ReactDOM.render(
+  <React.StrictMode>
     <Provider store={store}>
       <App />
-    </Provider>,
+    </Provider>
+  </React.StrictMode>,
   document.getElementById('root')
 );
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <Provider store={store}>
-//       <App />
-//     </Provider>
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
